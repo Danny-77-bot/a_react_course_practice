4d4fedee50a740310242e756cabfa5c9303fce61
Daniel <danielmelaku1177@gmail.com>
2025-02-02 23:59:26 +0300

    added input for useContext

--- src/compoents/auhtentication/LoginButton.jsx
+++ src/compoents/auhtentication/LoginButton.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+
 
 export default function LoginButton() {
   const { login } = useAuth();
+  const [username, setUsername] = useState("");
 
   return (
-    <button
-      onClick={() => login("Daniel")}
-      style={{ padding: "10px", margin: "10px", cursor: "pointer" }}
-    >
-      Log In
-    </button>
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button
+        onClick={() => login(username)}
+        disabled={!username.trim()}
+        style={{ padding: "10px", margin: "10px", cursor: "pointer" }}
+      >
+        Log In
+      </button>
+    </div>
   );
 }
